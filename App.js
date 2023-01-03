b8124d32eb25731e2f167f585b53e58b55787aa8
sizzle01 <talktoaliyu@gmail.com>
2023-01-02 21:35:21 -0500

    I added screen images

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Constants from 'expo-constants';
+import { Image, Platform } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeStackScreen from "./screens/RestaurantsScreen";
+// import { PeopleScreen } from "./screens/PeopleScreen";
+// import { DecisionScreen } from "./screens/DecisionScreen";
+import DecisionStackScreen from "./screens/DecisionScreen"
+
+
+
+
+
+console.log("------------------------------------------------------------");
+console.log(`RestaurantChooser starting on ${Platform.OS}`);
+
+
+// Normalized platform identifier to use when code needs to change between
+// Android and iOS.
+const platformOS = Platform.OS.toLowerCase();
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={{ headerShown: false }}
+      >
+        <Tab.Screen name="RestaurantsScreen" component={HomeStackScreen} 
+         options={({ route }) => ({
+          tabBarIcon: ({ tintColor }) => {
+            (
+              <Image source={ require("./images/icon-people.png") }
+                style={{ width : 32, height : 32, tintColor : tintColor }}
+              />
+              
+            )
+          },
+         
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'tomato',
+        })}
+        />
+        <Tab.Screen name="RestaurantsScreen" component={DecisionStackScreen} 
+         options={({ route }) => ({
+          tabBarIcon: ({ tintColor }) => {
+            (
+              <Image source={ require("./images/icon-decision.png") }
+                style={{ width : 32, height : 32, tintColor : tintColor }}
+              />
+            )
+          },
+         
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'tomato',
+        })}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
